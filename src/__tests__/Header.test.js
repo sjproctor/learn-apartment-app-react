21c4421852c35eed6642ea7187040dccd5571dbe
Sarah Proctor <sproctor950@gmail.com>
2024-04-08 13:37:00 -0700

    :art: reformats header test structure

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -2,25 +2,21 @@ import { render, screen } from "@testing-library/react"
 import { BrowserRouter } from "react-router-dom"
 import Header from "../components/Header"
 
-describe("Header component", () => {
-  test("renders logo and appropriate links", () => {
-    render(
-      <BrowserRouter>
-        <Header />
-      </BrowserRouter>
-    )
+test("renders Header component logo and appropriate links", () => {
+  render(
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
+  )
 
-    const logo = screen.getByAltText(
-      /stylized outline of house with two trees/i
-    )
-    expect(logo).toBeInTheDocument()
+  const logo = screen.getByAltText(/stylized outline of house with two trees/i)
+  expect(logo).toBeInTheDocument()
 
-    const availableApartmentLink = screen.getByText(/available apartments/i)
-    expect(availableApartmentLink).toBeInTheDocument()
-    expect(availableApartmentLink.getAttribute("href")).toBe("/")
+  const availableApartmentLink = screen.getByText(/available apartments/i)
+  expect(availableApartmentLink).toBeInTheDocument()
+  expect(availableApartmentLink.getAttribute("href")).toBe("/")
 
-    const signInLink = screen.getByText(/sign in/i)
-    expect(signInLink).toBeInTheDocument()
-    expect(signInLink.getAttribute("href")).toBe("/")
-  })
+  const signInLink = screen.getByText(/sign in/i)
+  expect(signInLink).toBeInTheDocument()
+  expect(signInLink.getAttribute("href")).toBe("/")
 })
