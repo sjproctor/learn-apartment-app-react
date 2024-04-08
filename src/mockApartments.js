8dc08b9e8f515bc94ef5b127ac024b1ce6347c5f
Sarah Proctor <sproctor950@gmail.com>
2024-04-08 16:31:46 -0700

    :seedling: update seed files for better testing

--- src/mockApartments.js
+++ src/mockApartments.js
@@ -3,34 +3,34 @@ const mockApartments = [
     id: 1,
     street: "101 Rimrock",
     unit: "9A",
-    city: "San Diego",
-    state: "CA",
+    city: "Austin",
+    state: "TX",
     square_footage: 500,
     price: "1800",
-    bedrooms: 2,
+    bedrooms: 3,
     bathrooms: 2.0,
     pets: "yes!",
     image:
-      "https://images.unsplash.com/photo-1469022563428-aa04fef9f5a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80",
+      "https://images.unsplash.com/photo-1602872030490-4a484a7b3ba6?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1173&q=80",
     user_id: 1
   },
   {
     id: 2,
     street: "720 Second Avenue",
     unit: "508",
-    city: "San Diego",
-    state: "CA",
+    city: "Boise",
+    state: "ID",
     square_footage: 700,
     price: "2500",
     bedrooms: 2,
     bathrooms: 2.0,
     pets: "yes!",
     image:
-      "https://images.unsplash.com/photo-1469022563428-aa04fef9f5a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80",
+      "https://images.unsplash.com/photo-1617806118233-18e1de247200?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     user_id: 1
   },
   {
-    id: 2,
+    id: 3,
     street: "415 Rainbow Street",
     unit: "3B",
     city: "San Diego",
@@ -41,7 +41,7 @@ const mockApartments = [
     bathrooms: 2.0,
     pets: "small pets okay",
     image:
-      "https://images.unsplash.com/photo-1469022563428-aa04fef9f5a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80",
+      "https://images.unsplash.com/photo-1617098709804-705581f844eb?q=80&w=1664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     user_id: 2
   }
 ]
