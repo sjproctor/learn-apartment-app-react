afda30858168949059d8c46a866afd1b9f185e34
Sarah Proctor <sproctor950@gmail.com>
2024-04-08 21:44:13 -0700

    :seedling: updates pet status structure

--- src/mockApartments.js
+++ src/mockApartments.js
@@ -9,7 +9,7 @@ const mockApartments = [
     price: "1800",
     bedrooms: 3,
     bathrooms: 2.0,
-    pets: "yes!",
+    pets: "pets okay with deposit",
     image:
       "https://images.unsplash.com/photo-1602872030490-4a484a7b3ba6?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1173&q=80",
     user_id: 1
@@ -24,7 +24,7 @@ const mockApartments = [
     price: "2500",
     bedrooms: 2,
     bathrooms: 2.0,
-    pets: "yes!",
+    pets: "cats only",
     image:
       "https://images.unsplash.com/photo-1617806118233-18e1de247200?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     user_id: 1
@@ -39,7 +39,7 @@ const mockApartments = [
     price: "2000",
     bedrooms: 1,
     bathrooms: 2.0,
-    pets: "small pets okay",
+    pets: "small pets okay with deposit",
     image:
       "https://images.unsplash.com/photo-1617098709804-705581f844eb?q=80&w=1664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     user_id: 2
