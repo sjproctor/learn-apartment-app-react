4e26f0665f6c411b05a5a65f75fdbd40999dd747
Sarah Proctor <sproctor950@gmail.com>
2024-04-15 20:55:15 -0700

    :bug: adds conditional rendering for apartment attributes

--- src/pages/ApartmentShow.js
+++ src/pages/ApartmentShow.js
@@ -7,7 +7,7 @@ import { ImLocation2 } from "react-icons/im"
 
 const ApartmentShow = ({ apartments }) => {
   const { id } = useParams()
-  const apartment = apartments.find((item) => item.id === +id)
+  const apartment = apartments.find((apt) => apt.id === +id)
 
   return (
     <>
@@ -15,37 +15,37 @@ const ApartmentShow = ({ apartments }) => {
       <div className="show-apartment">
         <div>
           <img
-            src={apartment.image}
+            src={apartment?.image}
             alt="apartment"
             className="show-apartment-image"
           />
         </div>
         <div className="apartment-details">
-          <h5>${apartment.price}</h5>
+          <h5>${apartment?.price}</h5>
           <p>
             <span className="icon">
               <ImLocation2 />
             </span>
-            {apartment.street}, {apartment.unit}, {apartment.city},{" "}
-            {apartment.state}
+            {apartment?.street}, {apartment?.unit}, {apartment?.city},{" "}
+            {apartment?.state}
           </p>
           <p>
             <span className="icon">
               <FaBed />
             </span>
-            {apartment.bedrooms} bedrooms | {apartment.bathrooms} bathrooms
+            {apartment?.bedrooms} bedrooms | {apartment?.bathrooms} bathrooms
           </p>
           <p>
             <span className="icon">
               <FaHouse />
             </span>
-            {apartment.square_footage} square feet
+            {apartment?.square_footage} square feet
           </p>
           <p>
             <span className="icon">
               <FaDog />
             </span>
-            Pet policy: {apartment.pets}
+            Pet policy: {apartment?.pets}
           </p>
         </div>
       </div>
